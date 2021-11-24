094322b8ceabf567f411b8b18317ad5d1496c57e
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-24 13:39:57 +0530

    add: added logic for digital root

---
+++ asg6/DigitalRoot.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+  int num, temp, sum;
+  scanf("%d", &num);
+
+  while (num > 9)
+  {
+    temp = num;
+    sum = 0;
+
+    if (temp < 0)
+    {
+      printf("%d is a magic number!\n", num);
+      break;
+    }
+
+    while (temp > 0)
+    {
+      sum += (temp % 10);
+      temp /= 10;
+    }
+    num = sum;
+  }
+  printf("Digital root value is: %d", num);
+  return 0;
+}
