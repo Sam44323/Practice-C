76033c3e6dde0eb3ae3258bf95998b6df9099f40
Suranjan <suranjanbiswas2002@gmail.com>
2021-11-24 13:50:50 +0530

    add: added prog for prime factors for a number

---
+++ asg6/Primefactors.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+  int i, j, num, prime_counter;
+
+  printf("Enter a number to print Prime factors: ");
+  scanf("%d", &num);
+
+  printf("All Prime Factors of %d are: \n", num);
+
+  for (i = 2; i <= num; i++)
+  {
+    if (num % i == 0)
+    {
+      prime_counter = 1;
+      for (j = 2; j <= i / 2; j++)
+      {
+        if (i % j == 0)
+        {
+          prime_counter = 0;
+          break;
+        }
+      }
+
+      if (prime_counter == 1)
+      {
+        printf("%d, ", i);
+      }
+    }
+  }
+
+  return 0;
+}
