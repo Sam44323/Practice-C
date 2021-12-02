77caeb3879add049e90241215f37e2e5b0a4a55a
Suranjan <suranjanbiswas2002@gmail.com>
2021-12-02 12:42:21 +0530

    add: added non fibo

--- asg9/function-switcher.c
+++ asg9/function-switcher.c
@@ -30,6 +30,35 @@ void Swapper()
   printf("Swapped! First: %d, Second: %d \n", n1, n2);
 }
 
+void NonFibonacci()
+{
+  int range, a, b, c, i, d;
+
+  a = 0;
+  b = 1;
+  c = 0;
+  printf("Enter the upper range of the series: \n");
+  scanf("%d", &range);
+
+  printf("Here is the series: \n");
+
+  while (c <= range)
+  {
+    c = a + b;
+    a = b;
+    b = c;
+    d = a + b;
+
+    for (i = c + 1; i < d; i++)
+    {
+      if (i <= range)
+        printf("%d \n", i);
+      else
+        break;
+    }
+  }
+}
+
 int main()
 {
   int choice = 0;
@@ -48,6 +77,10 @@ int main()
 
     case 2:
       Swapper();
+      break;
+
+    case 3:
+      NonFibonacci();
       break;
 
     default:
