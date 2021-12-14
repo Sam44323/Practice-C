fbd71d34a93f09785c2085dcf2da20d8edd08bd6
Suranjan <suranjanbiswas2002@gmail.com>
2021-12-14 11:39:32 +0530

    add: added transpose for matrix

---
+++ asg12/matrix-Transpose.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int main()
+{
+  int a[10][10], convertedMatrix[10][10], rows, cols;
+  printf("Enter rows and columns value for the matrix: ");
+  scanf("%d %d", &rows, &cols);
+
+  printf("Enter matrix elements:\n");
+  for (int i = 0; i < rows; ++i)
+    for (int j = 0; j < cols; ++j)
+    {
+      printf("Enter element at %d and %d position: ", i + 1, j + 1);
+      scanf("%d", &a[i][j]);
+    }
+
+  printf("Entered matrix: \n");
+  for (int i = 0; i < rows; ++i)
+    for (int j = 0; j < cols; ++j)
+    {
+      printf("%d  ", a[i][j]);
+      if (j == cols - 1)
+        printf("\n");
+    }
+
+  for (int i = 0; i < rows; ++i)
+    for (int j = 0; j < cols; ++j)
+    {
+      convertedMatrix[j][i] = a[i][j];
+    }
+
+  printf("\nTranspose of the matrix:\n");
+  for (int i = 0; i < cols; ++i)
+    for (int j = 0; j < rows; ++j)
+    {
+      printf("%d  ", convertedMatrix[i][j]);
+      if (j == rows - 1)
+        printf("\n");
+    }
+  return 0;
+}
