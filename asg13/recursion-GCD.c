caed59a3b73582a1bc3cdc3fcefcdf95b36239e7
Suranjan <suranjanbiswas2002@gmail.com>
2021-12-16 12:10:10 +0530

    add: added gcd calculator

---
+++ asg13/recursion-GCD.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int gcd(int n1, int n2)
+{
+  if (n2 == 0)
+  {
+    return n1;
+  }
+  else
+  {
+    return gcd(n2, n1 % n2);
+  }
+}
+
+int main()
+{
+  int n1, n2;
+  printf("Enter first positive number: \n");
+  scanf("%d", &n1);
+  printf("Enter second positive number: \n");
+  scanf("%d", &n2);
+
+  printf("GCD of %d and %d is %d \n", n1, n2, gcd(n1, n2));
+  return 0;
+}
