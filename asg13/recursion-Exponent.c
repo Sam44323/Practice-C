a8aa2153ad200fb541e3fdc2ab7f4d9742bacf47
Suranjan <suranjanbiswas2002@gmail.com>
2021-12-16 12:38:29 +0530

    add: added recursive exponent

---
+++ asg13/recursion-Exponent.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int calcExp(int n, int e)
+{
+  if (e == 0)
+    return 0;
+  else if (e == 1)
+    return n;
+  else
+    return n * calcExp(n, e - 1);
+}
+
+int main()
+{
+  int n, e;
+  printf("Enter the number: \n");
+  scanf("%d", &n);
+
+  printf("Enter the exponent: \n");
+  scanf("%d", &e);
+  printf("Exponent value for %d is %d", n, calcExp(n, e));
+  return 0;
+}
