f72cd53a2d620539a23049f5ea86ceafa7228a43
Suranjan <suranjanbiswas2002@gmail.com>
2022-03-05 15:16:24 +0530

    add: added code for reversing

--- final-assign/prog11.c
+++ final-assign/prog11.c
@@ -147,7 +147,20 @@ void sortingAscDesc(int *arr, int size)
   }
 }
 
-void reverseArray(int *arr, int size) {}
+void reverseArray(int *arr, int size)
+{
+  int temp, i = 0;
+  for (int i = 0; i < size / 2; i++)
+  {
+    temp = arr[i];
+    arr[i] = arr[size - i - 1];
+    arr[size - i - 1] = temp;
+  }
+
+  printf("Array after reversing: ");
+  for (i = 0; i < size; i++)
+    printf("%d ", arr[i]);
+}
 
 void frequencyCalculation(int *arr, int size) {}
 
