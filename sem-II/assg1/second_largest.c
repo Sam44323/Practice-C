9b27b7c34ea1294043ccf3f2c4e8e358f9ab5612
Suranjan <suranjanbiswas2002@gmail.com>
2022-03-15 13:52:02 +0530

    add: added code for second_largest

---
+++ sem-II/assg1/second_largest.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+#define MAX_SIZE 100
+
+int main()
+{
+  int arr[MAX_SIZE], size, i;
+  int max1, max2;
+
+  printf("Enter size of the array (1-100): ");
+  scanf("%d", &size);
+
+  printf("Enter elements in the array: ");
+  for (i = 0; i < size; i++)
+  {
+    scanf("%d", &arr[i]);
+  }
+
+  max1 = max2 = 0;
+
+  for (i = 0; i < size; i++)
+  {
+    if (arr[i] > max1)
+    {
+      max2 = max1;
+      max1 = arr[i];
+    }
+    else if (arr[i] > max2 && arr[i] < max1)
+    {
+      max2 = arr[i];
+    }
+  }
+
+  printf("Second largest = %d", max2);
+
+  return 0;
+}
