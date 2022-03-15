c4c316b56d364b760a85cf0e3d2345edb96ee124
Suranjan <suranjanbiswas2002@gmail.com>
2022-03-15 13:45:04 +0530

    add: added code for perfect_number_range

---
+++ sem-II/assg1/perfectNumberRange.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void main()
+{
+  int n, i, sum;
+  int min, max;
+  printf("Enter the starting range or number : ");
+  scanf("%d", &min);
+  printf("Enter the ending range of number : ");
+  scanf("%d", &max);
+  printf("The perfect numbers within the given range : ");
+  for (n = min; n <= max; n++)
+  {
+    i = 1;
+    sum = 0;
+    while (i < n)
+    {
+      if (n % i == 0)
+        sum = sum + i;
+      i++;
+    }
+    if (sum == n)
+      printf("%d ", n);
+  }
+  printf("\n");
+}
