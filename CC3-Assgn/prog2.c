6969d1ad74780977579d3a00e1321717c9c4d72a
Suranjan <suranjanbiswas2002@gmail.com>
2022-06-17 21:45:13 +0530

    update: updated the method_structure

---
+++ CC3-Assgn/prog2.c
@@ -0,0 +1,12 @@
+// sort using insertion, quick and radix sort
+
+#include <stdio.h>
+
+void insertion_sort(int *, int);
+void quick_sort(int *, int, int);
+void radix_sort(int *, int);
+
+int main()
+{
+  return 0;
+}
