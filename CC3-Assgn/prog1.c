db7d8303cedcd5181aa79063f909d6a570a3df73
Suranjan <suranjanbiswas2002@gmail.com>
2022-06-17 21:31:19 +0530

    add: added code for prog-1

---
+++ CC3-Assgn/prog1.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+
+void linear_search(int *, int, int);
+void binary_search(int *, int, int);
+
+void linear_search(int *arr, int n, int key)
+{
+  int i;
+  for (i = 0; i < n; i++)
+  {
+    if (arr[i] == key)
+    {
+      printf("%d found at index %d\n", key, i);
+      break;
+    }
+  }
+  if (i == n)
+  {
+    printf("%d not found\n", key);
+  }
+}
+
+void binary_search(int *arr, int n, int key)
+{
+  int low = 0, high = n - 1, mid;
+  while (low <= high)
+  {
+    mid = (low + high) / 2;
+    if (arr[mid] == key)
+    {
+      printf("%d found at index %d\n", key, mid);
+      break;
+    }
+    else if (arr[mid] < key)
+    {
+      low = mid + 1;
+    }
+    else
+    {
+      high = mid - 1;
+    }
+  }
+  if (low > high)
+  {
+    printf("%d not found\n", key);
+  }
+}
+
+int main()
+{
+  int n, key = 0, i, ele;
+  printf("Enter the number of elements: ");
+  scanf("%d", &n);
+  int arr[n];
+  printf("Enter the elements: ");
+  for (i = 0; i < n; i++)
+  {
+    scanf("%d", &arr[i]);
+  }
+  while (key != -1)
+  {
+    printf("Enter 1 for linear search | 2 for binary search | 3 for exit \n");
+    printf("Enter the key: ");
+    scanf("%d", &key);
+    printf("Enter the searching element: ");
+    scanf("%d", &ele);
+
+    switch (key)
+    {
+    case 1:
+      linear_search(arr, n, ele);
+      break;
+
+    case 2:
+      binary_search(arr, n, ele);
+      break;
+
+    default:
+      break;
+    }
+  }
+  return 0;
+}
